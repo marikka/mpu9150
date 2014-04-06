a5894027463d53985d37b6ad0665e53e5938e903
shrkey <barry@shrkey.com>
2014-04-06 14:06:49 +0100

    Added test function

---
+++ test.js
@@ -0,0 +1,13 @@
+var mpu9150 = require('./index.js');
+
+var imu = new mpu9150();
+imu.initialize();
+
+if( imu.testConnection() ) {
+	
+	setInterval( function() {
+		console.log(imu.getMotion6());
+	}, 1000 );
+}
+
+//imu.setSleepEnabled(1);
